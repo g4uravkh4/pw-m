eebd6820657540a99e9b14be0bff2e4433d9dd99
aura-gv <gauravkhatiwada4@gmail.com>
2023-08-23 16:31:47 +0545

    initial- setup firebase and tailwind

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'pw-manager-3169e',
+    appId: '1:46208864525:web:f7e41b1a10414f61486044',
+    storageBucket: 'pw-manager-3169e.appspot.com',
+    apiKey: 'AIzaSyAZaW4__3bbcWL3wLO03e_bu2coQEOmRK4',
+    authDomain: 'pw-manager-3169e.firebaseapp.com',
+    messagingSenderId: '46208864525',
+  },};
