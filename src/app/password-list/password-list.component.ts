ce565864e22b9c728d59d4ba1d638f4cd55c871c
aura-gv <gauravkhatiwada4@gmail.com>
2023-08-29 20:26:20 +0545

    pw list, form, passing data via query params

--- src/app/password-list/password-list.component.ts
+++ src/app/password-list/password-list.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-password-list',
   templateUrl: './password-list.component.html',
-  styleUrls: ['./password-list.component.css']
+  styleUrls: ['./password-list.component.css'],
 })
 export class PasswordListComponent {
+  siteId!: string;
+  siteName!: string;
+  siteUrl!: string;
+  siteImgUrl!: string;
 
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((val: any) => {
+      console.log(val);
+      this.siteId = val.id;
+      this.siteName = val.siteName;
+      this.siteUrl = val.siteUrl;
+      this.siteImgUrl = val.siteImageUrl;
+    });
+  }
+
+  onSubmit(values: object) {
+    console.log(values);
+  }
 }
